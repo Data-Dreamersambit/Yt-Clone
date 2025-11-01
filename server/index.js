a738a5e457cf134f9ffec665b2e5a45239a5ba9a
Sambit Sahoo <sambitsahoo191@gmail.com>
2025-11-01 06:32:51 +0530

    port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 connectDB();
 
 // Start the server
-const PORT = 5004;
+const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}!`);
 });
